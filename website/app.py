35055070ac87ee8c47c69766e1279effcf86dd45
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-11-17 19:08:38 -0300

    Disable flask debug mode

--- website/app.py
+++ website/app.py
@@ -3,5 +3,5 @@ from controllers.app_controller import create_app
 
 if __name__ == "__main__":
     app = create_app(os.path.abspath(os.path.join(os.path.dirname(__file__))))
-    
-    app.run(debug=True)
+
+    app.run()
