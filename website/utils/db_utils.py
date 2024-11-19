358fe0535c5c9672c132f0f39b66bd01bec04396
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-11-18 22:21:12 -0300

    Add test data to sensors_history

--- website/utils/db_utils.py
+++ website/utils/db_utils.py
@@ -65,6 +65,13 @@ def create_db(recreate=False):
         start_query("INSERT INTO sensor (id, location, type) VALUES (2, 'Poço das capivaras', 'Infravermelho')")
         start_query("INSERT INTO turnout (id, left_angle, right_angle) VALUES (1, 0, 60)")
         start_query("INSERT INTO turnout (id, left_angle, right_angle) VALUES (2, 60, 0)")
+
+        start_query("INSERT INTO sensors_history(value, datetime, sensor_id, actuator_id, type, description) VALUES ('True', '2024-11-18 22:12:08', 2, NULL, 'SENSOR', 'IR')")
+        start_query("INSERT INTO sensors_history(value, datetime, sensor_id, actuator_id, type, description) VALUES ('False', '2024-11-18 22:12:09', 2, NULL, 'SENSOR', 'IR')")
+        start_query("INSERT INTO sensors_history(value, datetime, sensor_id, actuator_id, type, description) VALUES ('4025', '2024-11-18 22:12:33', 1, NULL, 'SENSOR', 'RFID')")
+        start_query("INSERT INTO sensors_history(value, datetime, sensor_id, actuator_id, type, description) VALUES ('0', '2024-11-18 22:15:00', NULL, 1, 'ATUADOR', 'SERVO')")
+        start_query("INSERT INTO sensors_history(value, datetime, sensor_id, actuator_id, type, description) VALUES ('60', '2024-11-18 22:15:00', NULL, 2, 'ATUADOR', 'SERVO')")
+
         db.commit()
 
     cursor.close()
